89f4a7465693f750cf9e971893ffdebe5c7aac70
Ben Kunkle <17836474+probably-neb@users.noreply.github.com>
2021-05-28 22:49:07 -0500

    fish config

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,196 @@
+### fish config for neb ###
+
+set fish_greeting
+set TERM "xterm-256color"
+set -e fish_user_paths
+set -U fish_user_paths $HOME/.local/bin $HOME/Applications $HOME/.cargo/bin/ $fish_user_paths
+
+#fix gpg
+set -x GPG_TTY (tty)
+
+#blue "6088ff"
+#cyan "78dce8"
+#gray "6c6c6c"
+#a9dc76
+#green ( "61b948")
+#peach ( "ffbe63")
+#red ( "fe6188")
+#yellow ( "ffd866")
+#cyan1 (  "2cc6b5")
+#yellow1 (  "fcb726")
+#neutral_red (  "e64569")
+#blue1 (  "079ce0")
+#green1 ( "ffd952")
+#normal_pink ( "ff89c6")
+#pink ( "f47693")
+#light_grey_autocorrect ( "7d7d7d")
+#-gx commandtan ( "d6ccb0")
+#blue_param ( "eda2d1")
+#dark tan b6ad84
+
+set blue (set_color '6088ff') #blue
+set red (set_color 'e64569') #neutral red
+set pink (set_color 'cyan') # pink
+set -U fish_color_autosuggestion brred
+set -U fish_color_normal brgreen
+set -U fish_color_param bryellow
+set -U fish_color_search_match white --background=magenta
+set -U fish_color_operator normal
+set -U fish_color_valid_path normal
+set -U fish_pager_color_progress white --background=cyan
+set -U fish_pager_color_description normal
+set -U fish_pager_color_prefix cyan #blue param
+set -U fish_color_description normal
+set -U fish_color_selection brred --bold --background=black
+set -U fish_color_comment brblack
+set -U fish_color_escape brblack
+set -U fish_color_user green
+set -U fish_color_match bryellow
+set -U fish_color_operator brblack
+set -U fish_color_quote brblack
+set -U fish_color_redirection green
+
+
+### SPARK ###
+set -g spark_version 1.0.0
+
+complete -xc spark -n __fish_use_subcommand -a --help -d "Show usage help"
+complete -xc spark -n __fish_use_subcommand -a --version -d "$spark_version"
+complete -xc spark -n __fish_use_subcommand -a --min -d "Minimum range value"
+complete -xc spark -n __fish_use_subcommand -a --max -d "Maximum range value"
+
+function spark -d "sparkline generator"
+    if isatty
+        switch "$argv"
+            case {,-}-v{ersion,}
+                echo "spark version $spark_version"
+            case {,-}-h{elp,}
+                echo "usage: spark [--min=<n> --max=<n>] <numbers...>  Draw sparklines"
+                echo "examples:"
+                echo "       spark 1 2 3 4"
+                echo "       seq 100 | sort -R | spark"
+                echo "       awk \\\$0=length spark.fish | spark"
+            case \*
+                echo $argv | spark $argv
+        end
+        return
+    end
+
+    command awk -v FS="[[:space:],]*" -v argv="$argv" '
+        BEGIN {
+            min = match(argv, /--min=[0-9]+/) ? substr(argv, RSTART + 6, RLENGTH - 6) + 0 : ""
+            max = match(argv, /--max=[0-9]+/) ? substr(argv, RSTART + 6, RLENGTH - 6) + 0 : ""
+        }
+        {
+            for (i = j = 1; i <= NF; i++) {
+                if ($i ~ /^--/) continue
+                if ($i !~ /^-?[0-9]/) data[count + j++] = ""
+                else {
+                    v = data[count + j++] = int($i)
+                    if (max == "" && min == "") max = min = v
+                    if (max < v) max = v
+                    if (min > v ) min = v
+                }
+            }
+            count += j - 1
+        }
+        END {
+            n = split(min == max && max ? "▅ ▅" : "▁ ▂ ▃ ▄ ▅ ▆ ▇ █", blocks, " ")
+            scale = (scale = int(256 * (max - min) / (n - 1))) ? scale : 1
+            for (i = 1; i <= count; i++)
+                out = out (data[i] == "" ? " " : blocks[idx = int(256 * (data[i] - min) / scale) + 1])
+            print out
+        }
+    '
+end
+### END OF SPARK ###f
+
+# Changing "ls" to "exa"
+alias ls='exa -a --color=always --group-directories-first --no-user' # my preferred listing
+alias ld='exa -aD --color=always --group-directories-first --no-user'  # only directories
+alias ll='exa -al --color=always --group-directories-first --no-user'  # long format
+alias lt='exa -aTL 2 --color=always --group-directories-first --no-user' # tree listing
+alias l.='exa -a | egrep "^\."'
+
+
+# Functions needed for !! and !$
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+# Functions needed for !! and !$
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+# The bindings for !! and !$
+if [ $fish_key_bindings = fish_vi_key_bindings ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
+# Function for creating a backup file
+# ex: backup file.txt
+# result: copies file as file.txt.bak
+function backup --argument filename
+    cp $filename $filename.bak
+end
+
+
+# Function to identify colors being used #
+function print_fish_colors --description 'Shows the various fish colors being used'
+    set -l clr_list (set -n | grep fish | grep color | grep -v __)
+    if test -n "$clr_list"
+        set -l bclr (set_color normal)
+        set -l bold (set_color --bold)
+        printf "\n| %-38s | %-38s |\n" Variable Definition
+        echo '|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|'
+        for var in $clr_list
+            set -l def $$var
+            set -l clr (set_color $def ^/dev/null)
+            or begin
+                printf "| %-38s | %s%-38s$bclr |\n" "$var" (set_color --bold white --background=red) "$def"
+                continue
+            end
+            printf "| $clr%-38s$bclr | $bold%-38s$bclr |\n" "$var" "$def"
+        end
+        echo '|________________________________________|________________________________________|'\n
+    end
+end
+
+function in_font --description "installs a font file"
+  mv $argv ~/.local/share/fonts/
+  fc-cache -f -v
+end
+
+alias fishconfig='cd ~/.config/fish/'
+
+#function bwu --description 'Sets bitwarden session var after successful login'
+  #  bash -c '
+ #   BW_SESSION="$(bw unlock --raw)"'
+#end
+
+starship init fish | source
