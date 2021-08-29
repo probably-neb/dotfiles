4f99905f66b4c334b467eb850570e60ca6d98043
Ben Kunkle <ben.kunkle@gmail.com>
2021-08-29 14:50:53 -0500

    Initial Commit. Code, fish, kitty, and bashrc

---
+++ fish/config.fish
@@ -0,0 +1,186 @@
+### fish config for neb ###
+
+
+set fish_greeting
+set TERM "xterm-256color"
+set -e fish_user_paths
+set -U fish_user_paths $HOME/.local/bin $HOME/Applications $HOME/.cargo/bin/ $fish_user_paths
+
+#fix gpg
+set -x GPG_TTY (tty)
+
+#custom completions
+complete -xc chezmoi -n " __fish_seen_subcommand_from edit" -a "(chezmoi managed)"
+
+### SPARK ###
+set -g spark_version 1.0.0
+
+complete -xc spark -n __fish_use_subcommand -a --help -d "Show usage help"
+complete -xc spark -n __fish_use_subcommand -a --version -d "$spark_version"
+complete -xc spark -n __fish_use_subcommand -a --min -d "Minimum range value"
+complete -xc spark -n __fish_use_subcommand -a --max -d "Maximum range value"
+
+function spark -d "sparkline generator"
+    if isatty
+        switch "$argv"
+            case {,-}-v{ersion,}
+                echo "spark version $spark_version"
+            case {,-}-h{elp,}
+                echo "usage: spark [--min=<n> --max=<n>] <numbers...>  Draw sparklines"
+                echo "examples:"
+                echo "       spark 1 2 3 4"
+                echo "       seq 100 | sort -R | spark"
+                echo "       awk \\\$0=length spark.fish | spark"
+            case \*
+                echo $argv | spark $argv
+        end
+        return
+    end
+
+    command awk -v FS="[[:space:],]*" -v argv="$argv" '
+        BEGIN {
+            min = match(argv, /--min=[0-9]+/) ? substr(argv, RSTART + 6, RLENGTH - 6) + 0 : ""
+            max = match(argv, /--max=[0-9]+/) ? substr(argv, RSTART + 6, RLENGTH - 6) + 0 : ""
+        }
+        {
+            for (i = j = 1; i <= NF; i++) {
+                if ($i ~ /^--/) continue
+                if ($i !~ /^-?[0-9]/) data[count + j++] = ""
+                else {
+                    v = data[count + j++] = int($i)
+                    if (max == "" && min == "") max = min = v
+                    if (max < v) max = v
+                    if (min > v ) min = v
+                }
+            }
+            count += j - 1
+        }
+        END {
+            n = split(min == max && max ? "▅ ▅" : "▁ ▂ ▃ ▄ ▅ ▆ ▇ █", blocks, " ")
+            scale = (scale = int(256 * (max - min) / (n - 1))) ? scale : 1
+            for (i = 1; i <= count; i++)
+                out = out (data[i] == "" ? " " : blocks[idx = int(256 * (data[i] - min) / scale) + 1])
+            print out
+        }
+    '
+end
+
+function sparkwave
+    for i in (seq 100)
+       for j in (seq (math $COLUMNS - 1))
+           math "ceil(6 * cos(($i + $j) * pi / 5))"
+       end | spark | read sparks
+       echo -n $sparks\r && sleep .05
+    end
+    echo
+end
+
+alias lolwave='sparkwave | lolcat -F '0.198' -p 3'
+
+function httpspark
+    bash ~/.config/fish/httpspark.sh
+end
+
+function wifispark
+    bash ~/.config/fish/wifispark.sh
+end
+
+function loadaverage
+   bash ~/.config/fish/loadaveragespark.sh
+end
+
+### END OF SPARK ###
+
+# Changing "ls" to "exa"
+alias ls='exa -a --color=always --group-directories-first --no-user' # my preferred listing
+alias ld='exa -aD --color=always --group-directories-first --no-user'  # only directories
+alias ll='exa -al --color=always --group-directories-first --no-user'  # long format
+alias lt='exa -aTL 2 --color=always --group-directories-first --no-user' # tree listing
+alias l.='exa -a | egrep "^\."'
+
+
+# Functions needed for !! and !$
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+# Functions needed for !! and !$
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+# The bindings for !! and !$
+if [ $fish_key_bindings = fish_vi_key_bindings ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
+# Function for creating a backup file
+# ex: backup file.txt
+# result: copies file as file.txt.bak
+function backup --argument filename
+    cp $filename $filename.bak
+end
+
+
+# Function to identify colors being used #
+function print_fish_colors --description 'Shows the various fish colors being used'
+    set -l clr_list (set -n | grep fish | grep color | grep -v __)
+    if test -n "$clr_list"
+        set -l bclr (set_color normal)
+        set -l bold (set_color --bold)
+        printf "\n| %-38s | %-38s |\n" Variable Definition
+        echo '|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|'
+        for var in $clr_list
+            set -l def $$var
+            set -l clr (set_color $def ^/dev/null)
+            or begin
+                printf "| %-38s | %s%-38s$bclr |\n" "$var" (set_color --bold white --background=red) "$def"
+                continue
+            end
+            printf "| $clr%-38s$bclr | $bold%-38s$bclr |\n" "$var" "$def"
+        end
+        echo '|________________________________________|________________________________________|'\n
+    end
+end
+
+function in_font --description "installs a font file"
+  mv $argv ~/.local/share/fonts/
+  fc-cache -f -v
+end
+
+#chezmoi helpers
+alias cm='chezmoi'
+alias fishconfig='nano ~/.config/fish/config.fish'
+alias chezrl='chezmoi managed | chezmoi re-add'
+alias alaconfig='code ~/.config/alacritty/alacritty.yml'
+
+#function bwu --description 'Sets bitwarden session var after successful login'
+  #  bash -c '
+ #   BW_SESSION="$(bw unlock --raw)"'
+#end
+
+starship init fish | source
