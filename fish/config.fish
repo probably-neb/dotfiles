c1408cb670ef499039c6e519753409a1df520c9d
Ben Kunkle <ben.kunkle@gmail.com>
2022-11-03 22:55:57 -0700

    installed lazygit -- add alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,3 @@
-# set -U fish_function_path "~/dotfiles/fish/functions/{,z/} /usr/share/fish/functions/ "
-# if not contains /home/neb/dotfiles/fish/functions/z $fish_function_path
-#  echo "adding z path"
-#  set -a fish_function_path ~/dotfiles/fish/functions/z
-# end
-### fish config for neb ###
 set -U fish_greeting (fortune)
 # set fish_greeting (checkupdates || true && yay -Qua)
 
@@ -11,29 +5,6 @@ set TERM "xterm-256color"
 
 #keybindings
 set -g fish_key_bindings fish_vi_key_bindings
-# set -e fish_user_paths
-# set -U fish_user_paths $HOME/.local/bin $HOME/Applications $HOME/.cargo/bin/ $fish_user_paths
-# fish_add_path --path '/home/neb/.ghcup/bin/ghcup'
-#
-# set -aU fish_function_path $HOME/.config/fish/functions/tide/ $HOME/.config/fish/functions/z/ $HOME/.config/fish/functions/fzf/ 
-#
-#
-
-set -g hydro_color_normal (set_color green)
-set -g hydro_color_path (set_color magenta)
-
-#function nebtheme -a theme -d "change kitty and nvim color schemes"
-#	set -l theme_path "/home/neb/dotfiles/kitty/themes/"$theme".conf"
-#	set -l image_path "/home/neb/dotfiles/kitty/"$theme".png"
-#  kitty @ --to=$KITTY_LISTEN_ON set-colors --all --configured $theme_path
-#	# why set-background-image no work?
-#	kitty @ --to=$KITTY_LISTEN_ON set-background-image none
-#	#set theme in global theme file
-#	echo $theme > /home/neb/dotfiles/.theme
-#end
-
-# set theme from global theme file
-# eval nebtheme (tr '[:upper:]' '[:lower:]' < /home/neb/dotfiles/.theme)
 
 #fix gpg
 set -x GPG_TTY (tty)
@@ -59,6 +30,7 @@ abbr --add v 'nvim'
 abbr --add cerrcodes 'sqlite3 ~/Documents/csc/errcodes.sqlite'
 abbr --add scpvim 'nvim scp://$unix1/'
 
+alias lg='lazygit'
 alias t='kitty @ launch --cwd=current --type=tab'
 #save a character when typing nvim because why not
 alias vim='nvim'
--- nvim/after/ftplugin/markdown/markdown.vim
+++ nvim/after/ftplugin/markdown/markdown.vim
@@ -1,2 +1,18 @@
 set spell spelllang=en_us
 set conceallevel=1
+
+function! SwapCheckBox()
+   let line = line('.')
+   let pos = getcurpos('.')
+   let cur = ' '
+   let new = 'x'
+   if match(getline(line), '\[x]') >= 0
+      let cur = 'x'
+      let new = ' '
+   endif
+   let cmd = [':',line,'s/\[', cur, ']/[', new, ']']
+   execute join(cmd, '')
+   call setpos('.', pos)
+endfunction
+
+nnoremap <C-m> :call SwapCheckBox()<CR>
