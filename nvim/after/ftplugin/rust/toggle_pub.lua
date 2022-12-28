4c57d20ce44cc951ead76ae8b3176f8a9566b086
Ben Kunkle <ben.kunkle@gmail.com>
2022-12-27 21:17:43 -0800

    nvim: rust ftplugin to toggle pub using Commment.nvim and setting the commentstring

---
+++ nvim/after/ftplugin/rust/toggle_pub.lua
@@ -0,0 +1,30 @@
+local ok, api = pcall(require, "Comment.api")
+if not ok then
+    return
+end
+
+-- returns a copy of the current config
+local get_toggle_config = function()
+    local config = require("Comment.config")
+    local pre_hook = function(_)
+        return "pub %s"
+    end
+    config.pre_hook = pre_hook
+    config.padding = true
+
+    return config
+end
+local toggle_pub_n = function()
+    local config = get_toggle_config()
+    api.toggle.linewise(nil, config)
+end
+local toggle_pub_v = function()
+    local config = get_toggle_config()
+    local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+    vim.api.nvim_feedkeys(esc, "nx", false)
+    api.toggle.linewise(vim.fn.visualmode(), config)
+end
+---- Toggle selection (linewise)
+vim.keymap.set("x", "<leader>p", toggle_pub_v, { silent = false })
+vim.keymap.set("n", "<Leader>p", toggle_pub_n, { silent = false })
+-- vim.keymap.set("v", "<Leader>p", toggle_pub_v, { silent = false })
