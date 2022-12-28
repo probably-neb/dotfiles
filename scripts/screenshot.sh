f3deefb8d345c6ea155374782e0c981b0a2d9009
Ben Kunkle <ben.kunkle@gmail.com>
2022-12-27 21:25:18 -0800

    script: feat: allow passing a path to the screenshot script

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -2,5 +2,8 @@
 
 selection=$(hacksaw -f "-i %i -g %g")
 imgpath="/home/neb/Pictures/$(date "+%Y-%m-%d-%T")-screenshot.png"
+if [[ $1 ]]; then
+    imgpath=$1
+fi
 shotgun $selection - | tee $imgpath | xclip -t 'image/png' -selection clipboard
 echo $imgpath
