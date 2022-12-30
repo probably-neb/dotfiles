bbeff2b217eda7c09ee515a04bc8d780c0a3eca6
Ben Kunkle <ben.kunkle@gmail.com>
2022-12-29 20:20:59 -0800

    nvim: feat tokyonight colorscheme (ily @folke) + some comments for future me

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,20 @@
--- vim: foldmethod=marker 
--- vim: foldlevel=0
+-- vim:foldmethod=marker:foldlevel=0
+
 -- nebvim
 
+-- some global settings I'd like applied everywhere
 vim.g.neb = { leader = ' ', indent = 4 }
+
+-- blazingly
 require('impatient')
 
 require('config.options')
 require('config.mappings')
 
-require('config.plugins')
+require('config.packer')
 
 vim.cmd [[ colorscheme base16 ]]
+vim.cmd [[ colorscheme tokyonight ]]
 
 -- autocmd Bufread,BufNewFile *.h set filetype=c 
 
@@ -18,6 +22,6 @@ function P(...)
     print(vim.inspect(...))
 end
 
--- FIXME: intro screen not showing. Shows with `nvim --noplugin`
+-- TODO: intro screen not showing. Shows with `nvim --noplugin`
 -- bandaid solution:
 -- vim.cmd [[ exec ":intro" ]]
