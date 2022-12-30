97378fbe6b3cc503b779fddaf3d199fe8e58389f
Ben Kunkle <ben.kunkle@gmail.com>
2022-12-29 21:25:38 -0800

    nvim: feat: organize? who am I?

--- nvim/lua/neb/config/packer.lua
+++ nvim/lua/neb/config/packer.lua
@@ -7,13 +7,15 @@ vim.cmd([[
 
 return require("packer").startup(function(use)
 	use("wbthomason/packer.nvim")
-
 	use("lewis6991/impatient.nvim")
-
-	use({ "/home/neb/code/nvim-rustdocs", rocks = "htmlparser" })
 
 	-- lsp, mason?, lints, daps, oh my!
 	use({
+		{ "neovim/nvim-lspconfig" },
+		{ "nvim-lua/lsp-status.nvim" },
+		{ "mfussenegger/nvim-dap" },
+		{ "glepnir/lspsaga.nvim" },
+
 		{ "jayp0521/mason-null-ls.nvim" },
 		{ "jose-elias-alvarez/null-ls.nvim" },
 		{ "williamboman/mason-lspconfig.nvim" },
@@ -39,17 +41,8 @@ return require("packer").startup(function(use)
 			end,
 		},
 	})
-	use({
-		"kylechui/nvim-surround",
-		config = function()
-			require("nvim-surround").setup()
-		end,
-	})
 
-	use("neovim/nvim-lspconfig")
-    use("nvim-lua/lsp-status.nvim")
-	use("mfussenegger/nvim-dap")
-	-- -- cmp stuff
+	-- cmp stuff
 	use({
 		"hrsh7th/nvim-cmp",
 		requires = {
@@ -58,141 +51,171 @@ return require("packer").startup(function(use)
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-cmdline",
 			"saadparwaiz1/cmp_luasnip",
-			-- "quangnguyen30192/cmp-nvim-ultisnips",
 			"uga-rosa/cmp-dictionary",
 			{ "mtoohey31/cmp-fish", ft = "fish" },
 		},
 	})
 
+	-- tools
 	use({
-		"lukas-reineke/indent-blankline.nvim",
-		config = function()
-			vim.opt.list = true
-			-- vim.opt.listchars:append "space:⋅"
-			require("indent_blankline").setup({
-				-- char = "",
-				space_char_blankline = " ",
-				show_trailing_blankline_indent = false,
-			})
-
-			if vim.g.colors_name == "base16" then
-				local color = vim.g.colors.base02
-				vim.cmd("highlight IndentBlanklineChar guifg=" .. color .. " gui=nocombine")
-				vim.cmd("highlight IndentBlanklineSpaceChar guifg=" .. color .. " gui=nocombine")
-			end
-		end,
+		{ "L3MON4D3/LuaSnip", tag = "v1.*" },
+		{
+			"chipsenkbeil/distant.nvim",
+			config = function()
+				require("distant").setup({
+					-- Applies Chip's personal settings to every machine you connect to
+					--
+					-- 1. Ensures that distant servers terminate with no connections
+					-- 2. Provides navigation bindings for remote directories
+					-- 3. Provides keybinding to jump into a remote file's parent directory
+					["*"] = require("distant.settings").chip_default(),
+					["arm"] = {
+						mode = "ssh",
+					},
+				})
+			end,
+		},
+		{ "mbbill/undotree" },
+		{ "nvim-telescope/telescope.nvim" },
 	})
-
-    use {
-        "nvim-lualine/lualine.nvim",
-        requires = { "kyazdani42/nvim-web-devicons"},
-    }
-
-	use({
-		"chipsenkbeil/distant.nvim",
-		config = function()
-			require("distant").setup({
-				-- Applies Chip's personal settings to every machine you connect to
-				--
-				-- 1. Ensures that distant servers terminate with no connections
-				-- 2. Provides navigation bindings for remote directories
-				-- 3. Provides keybinding to jump into a remote file's parent directory
-				["*"] = require("distant.settings").chip_default(),
-				["arm"] = {
-					mode = "ssh",
-				},
-			})
-		end,
-	})
-	-- use 'zbirenbaum/copilot.lua'
-	-- -- find way to load after
-	-- use 'zbirenbaum/copilot-cmp'
-
-	use("nvim-lua/plenary.nvim")
-
-	use("mbbill/undotree")
-
-	use("glepnir/lspsaga.nvim")
 
 	use({
 		{ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
 		"nvim-treesitter/playground",
 		"nvim-treesitter/nvim-treesitter-context",
 		"nvim-treesitter/nvim-treesitter-refactor",
+		"nvim-treesitter/nvim-treesitter-textobjects",
 		"windwp/nvim-ts-autotag",
 	})
 
-	use("nvim-telescope/telescope.nvim")
-
-	use("BurntSushi/ripgrep")
-
-	use("nvim-lua/popup.nvim")
-
+	-- utilities
 	use({
-		"windwp/nvim-autopairs",
-		config = function()
-			local autopairs = require("nvim-autopairs")
-			autopairs.setup()
-		end,
+		{
+			"windwp/nvim-autopairs",
+			config = function()
+				local autopairs = require("nvim-autopairs")
+				autopairs.setup()
+			end,
+		},
+		{
+			"kylechui/nvim-surround",
+			config = function()
+				require("nvim-surround").setup()
+			end,
+		},
+		{
+			-- god this plugin is fucking amazing
+			"wellle/targets.vim",
+		},
+		{
+			"numToStr/Comment.nvim",
+			config = function()
+				require("Comment").setup()
+				local api = require("Comment.api")
+				-- additional comment bindings (definitely not taken from vs****)
+				-- visual mode linewise
+				local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+				vim.keymap.set("x", "<C-/>", function()
+					vim.api.nvim_feedkeys(esc, "nx", false)
+					api.toggle.linewise(vim.fn.visualmode())
+				end)
+				-- visual mode blockwise
+				vim.keymap.set("x", "<C-\\>", function()
+					vim.api.nvim_feedkeys(esc, "nx", false)
+					api.toggle.blockwise(vim.fn.visualmode())
+				end)
+				vim.keymap.set("n", "<C-/>", api.toggle.linewise.current, { silent = false })
+			end,
+		},
+		{ "nvim-lua/plenary.nvim" },
+		{ "BurntSushi/ripgrep" },
 	})
-	-- god this plugin is fucking amazing
-	use("wellle/targets.vim")
 
-	-- tpope appreciation corner
+	-- asthetics
 	use({
+		{
+			"nvim-lualine/lualine.nvim",
+			requires = { "kyazdani42/nvim-web-devicons" },
+		},
+		{
+			"morhetz/gruvbox",
+			config = function()
+				vim.g.gruvbox_contrast_light = "hard"
+			end,
+			opt = true,
+		},
+		{
+			"lukas-reineke/indent-blankline.nvim",
+			config = function()
+				vim.opt.list = true
+				-- vim.opt.listchars:append "space:⋅"
+				require("indent_blankline").setup({
+					-- char = "",
+					space_char_blankline = " ",
+					show_trailing_blankline_indent = false,
+				})
+
+				if vim.g.colors_name == "base16" then
+					local color = vim.g.colors.base02
+					vim.cmd("highlight IndentBlanklineChar guifg=" .. color .. " gui=nocombine")
+					vim.cmd("highlight IndentBlanklineSpaceChar guifg=" .. color .. " gui=nocombine")
+				end
+			end,
+		},
+	})
+
+	-- filetype specific plugins
+	use({
+		{
+			"dag/vim-fish",
+			ft = { "fish" },
+		},
+		{ "simrat39/rust-tools.nvim" },
+		{
+			"lervag/vimtex",
+			config = function()
+				vim.cmd([[ let g:tex_flavor='latex' ]])
+				vim.cmd([[ let g:vimtex_view_method='zathura' ]])
+				vim.cmd([[ let g:vimtex_quickfix_mode=1 ]])
+			end,
+		},
+	})
+
+	-- @tpope appreciation corner
+	use({
+		-- this is because I can't spell
 		"tpope/vim-abolish",
 	})
 
-	-- folke appreciation corner
+	-- @folke appreciation corner
 	use({
-		"folke/todo-comments.nvim",
-		requires = "nvim-lua/plenary.nvim",
-		config = function()
-			require("todo-comments").setup({
-				signs = false,
-			})
-		end,
+		{
+			"folke/todo-comments.nvim",
+			requires = "nvim-lua/plenary.nvim",
+			config = function()
+				require("todo-comments").setup({
+					signs = false,
+				})
+			end,
+		},
+		{
+			"folke/tokyonight.nvim",
+		},
 	})
 
+	-- @ThePrimeagen appreciation corner
 	use({
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-			local api = require("Comment.api")
-			-- additional comment bindings (definitely not taken from vs****)
-			-- visual mode linewise
-			local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
-			vim.keymap.set("x", "<C-/>", function()
-				vim.api.nvim_feedkeys(esc, "nx", false)
-				api.toggle.linewise(vim.fn.visualmode())
-			end)
-			-- visual mode blockwise
-			vim.keymap.set("x", "<C-\\>", function()
-				vim.api.nvim_feedkeys(esc, "nx", false)
-				api.toggle.blockwise(vim.fn.visualmode())
-			end)
-			vim.keymap.set("n", "<C-/>", api.toggle.linewise.current, { silent = false })
-		end,
-	})
-
-	-- colorschems
-	use({
-		"morhetz/gruvbox",
-		config = function()
-			vim.g.gruvbox_contrast_light = "hard"
-		end,
+		"ThePrimeagen/vim-be-good",
 		opt = true,
+	}, {
+		"ThePrimeagen/harpoon",
 	})
-	use("folke/tokyonight.nvim")
 
-    -- filetype specific plugins
-	use({ "dag/vim-fish", ft = { "fish" } })
-    use 'fladson/vim-kitty'
+	-- this is why I am organizing right now
+	-- TODO: this is probably a dependency for something
+	-- use("nvim-lua/popup.nvim")
 
-	-- use {'simrat39/rust-tools.nvim', ft = {'rs'}}
-	use({ "simrat39/rust-tools.nvim" })
-
-	use({ "L3MON4D3/LuaSnip", tag = "v1.*" })
+	-- deprecated
 	-- use({
 	--     "SirVer/ultisnips",
 	--     config = function()
@@ -206,7 +229,7 @@ return require("packer").startup(function(use)
 	--     end,
 	-- })
 
-	use("cohama/lexima.vim")
+	-- use("cohama/lexima.vim")
 
 	-- use 'godlygeek/tabular'
 	-- let g:haskell_tabular = 1
@@ -214,23 +237,9 @@ return require("packer").startup(function(use)
 	-- vmap a; :Tabularize /::<CR>
 	-- vmap a- :Tabularize /-><CR> use 'dkarter/bullets.vim'
 
-	-- -- use 'kyazdani42/nvim-web-devicons'
-
-	-- prime corner
-	use({ "ThePrimeagen/vim-be-good", opt = true })
-	use({ "ThePrimeagen/harpoon" })
-
-	use({
-		"lervag/vimtex",
-		config = function()
-			vim.cmd([[ let g:tex_flavor='latex' ]])
-			vim.cmd([[ let g:vimtex_view_method='zathura' ]])
-			vim.cmd([[ let g:vimtex_quickfix_mode=1 ]])
-		end,
-		-- use 'KeitaNakamura/tex-conceal.vim'
-		--     let g:tex_conceal='abdmgs'
-		--     hi Conceal ctermbg=none
-	})
+	-- use 'KeitaNakamura/tex-conceal.vim'
+	--     let g:tex_conceal='abdmgs'
+	--     hi Conceal ctermbg=none
 end)
 
 -- vmap a= :Tabularize /=<CR>
