8bebc0fd0e7d7c87791051d50f96b246c252223e
Ben Kunkle <ben.kunkle@gmail.com>
2022-12-30 12:20:54 -0800

    nvim(feat): more telescope bindings

--- nvim/lua/neb/plugins/telescope.rc.lua
+++ nvim/lua/neb/plugins/telescope.rc.lua
@@ -1,31 +1,37 @@
 if vim.g.loaded_telescope ~= nil then
-  local opts = { noremap = true, silent = false}
-  local req = ':lua require("telescope.builtin").'
+	local opts = { noremap = true, silent = false }
+    local themes = require('telescope.themes')
+	local telescope_leader = "<leader>f"
 
-  local map = function(pre, key, finder)
-     vim.api.nvim_set_keymap('n', pre..key, req..finder..'()<cr>', opts)
-  end
+	local map = function(key, finder, picker_opts)
+        local map_func = function()
+            require('telescope.builtin')[finder](picker_opts)
+        end
+		vim.keymap.set("n", telescope_leader .. key, map_func, opts)
+	end
 
-  local pre = '<leader>f'
-  map('', '<leader>g', 'find_files')
-  map(pre, 'g', 'live_grep')
-  map(pre, 'b', 'buffers')
-  map(pre, 'h', 'help_tags')
-  map(pre, 'd', 'diagnostics')
-  map(pre, 'm', 'man_pages')
-  map(pre, 'z', 'current_buffer_fuzzy_find')
-  map(pre, 't', 'tags')
-  map(pre, 'a', 'builtin')
-  map(pre, 'l', 'treesitter')
-  -- Global remapping
-  ------------------------------
-  require('telescope').setup {
-    defaults = {
-      mappings = {
-        n = {
-          ["q"] = require('telescope.actions').close,
-        },
-      },
-    }
-  }
+	map( "f", "find_files", themes.get_ivy())
+	map( "g", "live_grep")
+	map( "b", "buffers")
+	map( "h", "help_tags")
+	map( "d", "diagnostics")
+	map( "m", "man_pages")
+	map( "z", "current_buffer_fuzzy_find")
+	map( "t", "tags")
+	map( "a", "builtin")
+	map( "l", "treesitter")
+    map( "rl", "reloader")
+    map( "c", "commands")
+    map( "k", "keymaps")
+	-- Global remapping
+	------------------------------
+	require("telescope").setup({
+		defaults = {
+			mappings = {
+				n = {
+					["q"] = require("telescope.actions").close,
+				},
+			},
+		},
+	})
 end
