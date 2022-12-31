b2e218523c2a7ba1f61d1052fd153b444539ff45
Ben Kunkle <ben.kunkle@gmail.com>
2022-12-30 20:13:06 -0800

    nvim(feat): more choices for making functions in lua

--- nvim/luasnippets/lua.lua
+++ nvim/luasnippets/lua.lua
@@ -35,9 +35,24 @@ end
 
 local func_node = function()
 	return {
-		t("function("),
-		i(1),
-		t(")"),
+        c(1,{
+            sn(nil, fmt(
+                "function({})",
+                i(1)
+            )),
+            sn(nil, fmt(
+                "function {}{}({})",
+                {
+                    c(1,{
+                        t"M.",
+                        t"",
+                        sn(nil, {i(1),t"."})
+                    }),
+                    i(2),
+                    i(3),
+                }
+            ))
+        }),
 		c(2, {
 			sn(nil, { t(" "), res(1, "body"), t(" ") }),
 			sn(nil, { t({ "", "\t" }), res(1, "body"), t({ "", "" }) }),
@@ -68,7 +83,7 @@ local last_item_in_lua_path = function(node)
 	req_path = vim.fn.substitute(req_path, "')", "", "")
 	local path = req_path
 	local no_regex = true
-	local modules = vim.split(path, ".", no_regex)
+	local modules = vim.split(path, ".", {plain = true})
 	local last_item = modules[#modules] or ""
 	last_item = vim.fn.substitute(last_item, "-", "_", "g")
 	return last_item
