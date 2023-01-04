c6a5eb0b7e04af90cf763e1c1dcc76097ed31d2b
Ben Kunkle <ben.kunkle@gmail.com>
2023-01-04 04:19:23 -0800

    nvim(feat): reuse old web-devicons config

---
+++ nvim/lua/neb/config/plugins/web-devicons.lua
@@ -0,0 +1,46 @@
+local M = {
+	"kyazdani42/nvim-web-devicons",
+}
+
+M.setup = function()
+	require("nvim-web-devicons").setup({
+		-- your personnal icons can go here (to override)
+		-- DevIcon will be appended to `name`
+		protocol = {
+			SymbolKind = {},
+			CompletionItemKind = {
+				"", -- Text
+				"", -- Method
+				"", -- Function
+				"", -- Constructor
+				"", -- Field
+				"", -- Variable
+				"", -- Class
+				"ﰮ", -- Interface
+				"", -- Module
+				"", -- Property
+				"", -- Unit
+				"", -- Value
+				"", -- Enum
+				"", -- Keyword
+				"﬌", -- Snippet
+				"", -- Color
+				"", -- File
+				"", -- Reference
+				"", -- Folder
+				"", -- EnumMember
+				"", -- Constant
+				"", -- Struct
+				"", -- Event
+				"ﬦ", -- Operator
+				"", -- TypeParameter
+			},
+		},
+		-- override = {},
+		-- globally enable default icons (default to false)
+		-- will get overriden by `get_icons` option
+		-- default = true,
+	})
+end
+
+return M
