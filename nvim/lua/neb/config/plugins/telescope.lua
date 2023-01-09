aeb86a8815c775c20acd3b694289b4db96ef68ee
Ben Kunkle <ben.kunkle@gmail.com>
2023-01-09 11:44:32 -0800

    feat(nvim-telescope): add bind to find files with same file extension

--- nvim/lua/neb/config/plugins/telescope.lua
+++ nvim/lua/neb/config/plugins/telescope.lua
@@ -1,36 +1,46 @@
 local telescope_leader = "<leader>f"
 
-local M = { "nvim-telescope/telescope.nvim",
-keys = {
-        telescope_leader
-    },
-    cmd = "Telescope",
-}
+local M = { "nvim-telescope/telescope.nvim", keys = {
+	telescope_leader,
+}, cmd = "Telescope" }
 
 M.config = function()
 	local opts = { noremap = true, silent = false }
-    local themes = require('telescope.themes')
 
 	local map = function(key, finder, picker_opts)
-        local map_func = function()
-            require('telescope.builtin')[finder](picker_opts)
-        end
+		local map_func = function()
+            local picker_opts = function()
+                if type(picker_opts) == "table" then
+                    return picker_opts
+                elseif type(picker_opts) == "function" then
+                    return picker_opts()
+                end
+            end
+			require("telescope.builtin")[finder](picker_opts())
+		end
 		vim.keymap.set("n", telescope_leader .. key, map_func, opts)
 	end
 
-	map( "f", "find_files", themes.get_ivy())
-	map( "g", "live_grep")
-	map( "b", "buffers")
-	map( "h", "help_tags")
-	map( "d", "diagnostics")
-	map( "m", "man_pages")
-	map( "z", "current_buffer_fuzzy_find")
-	map( "t", "tags")
-	map( "a", "builtin")
-	map( "l", "treesitter")
-    map( "rl", "reloader")
-    map( "c", "commands")
-    map( "k", "keymaps")
+	map("f", "find_files")
+    map("w", "find_files",
+        function()
+            return {
+                search_file = vim.fn.expand("%:e")
+            }
+        end)
+	map("s", "lsp_workspace_symbols")
+	map("g", "live_grep")
+	map("b", "buffers")
+	map("h", "help_tags")
+	map("d", "diagnostics")
+	map("m", "man_pages")
+	map("z", "current_buffer_fuzzy_find")
+	map("t", "tags")
+	map("a", "builtin")
+	map("l", "treesitter")
+	map("rl", "reloader")
+	map("c", "commands")
+	map("k", "keymaps")
 	-- Global remapping
 	------------------------------
 	require("telescope").setup({
@@ -41,6 +51,11 @@ M.config = function()
 				},
 			},
 		},
+        pickers = {
+            find_files = {
+                theme = "ivy"
+            }
+        }
 	})
 end
 
