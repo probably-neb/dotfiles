e8ff26db63e71e26362bcca91f4824f4e9200131
Ben Kunkle <ben.kunkle@gmail.com>
2023-02-21 21:21:09 -0800

    nvim: base16 color scheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,13 +5,14 @@
 -- blazingly
 -- require('impatient')
 
+
 require('neb.config.options')
 require('neb.config.mappings')
 -- require('neb.config.packer')
 require('neb.config.lazy')
 require('neb.config.commands')
 
--- vim.cmd [[ colorscheme base16 ]]
 -- vim.cmd [[ colorscheme tokyonight ]]
+vim.cmd [[ colorscheme base16 ]]
 
 -- autocmd Bufread,BufNewFile *.h set filetype=c 
