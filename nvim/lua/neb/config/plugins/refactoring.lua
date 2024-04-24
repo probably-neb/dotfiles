38bea0eff01b39611b8ede647beeb40f015294b9
Ben Kunkle <ben.kunkle@gmail.com>
2024-04-24 13:12:26 -0700

    nvim: get refactoring working a bit better

--- nvim/lua/neb/config/plugins/refactoring.lua
+++ nvim/lua/neb/config/plugins/refactoring.lua
@@ -1,7 +1,8 @@
+local refactoring_leader = "<leader>r"
 return {
 	-- dir = "~/code/exploring/refactoring.nvim",
-    "ThePrimeagen/refactoring.nvim",
-    dev =true,
+	"ThePrimeagen/refactoring.nvim",
+	-- dev = true,
 	ft = {
 		"typescript",
 		"javascript",
@@ -9,42 +10,65 @@ return {
 		"c",
 		"cpp",
 		"golang",
+		"go",
 		"python",
 		"java",
 		"php",
 		"ruby",
 		"rust",
 	},
+	keys = {},
 	config = function(opts)
 		require("refactoring").setup(opts)
-		local extract = function(what)
-			return { "<Esc><Cmd>lua require('refactoring').refactor('Extract " .. what .. "')<CR>", what }
-		end
-		local debug = function(what, ext,mode)
-			-- local ext = (mode == 'n') and "{ normal = true }" or "{}"
-			return { ":lua require('refactoring').debug." .. what .. "(" .. ext .. ")<CR>", what ,mode=mode} --, mode=mode}
-		end
-		local p = function() end
-        local modes = require('neb.config.plugins.wk').modes
+		local modes = require("neb.config.plugins.wk").modes
+
+		local refactoring = require("refactoring")
+
 		require("which-key").register({
 			name = "refactoring",
-			["<leader>r"] = {
-				e = {
-					f = extract("Function"),
-					v = extract("Variable"),
-					t = extract("Function To File"),
-                    mode = "v",
+			e = {
+				f = {
+					function()
+						refactoring.refactor("Extract Function")
+					end,
+					"Extract Function",
 				},
-				p = modes({
-						n = debug("print_var", "{normal = true}"),
-						v = debug("print_var", "{}"),
-				}),
-                -- {
-                --     debug("print_var", "{normal = true}","n"),
-                --     debug("print_var", "{}","v"),
-                -- }
+				ff = {
+					function()
+						refactoring.refactor("Extract Function To File")
+					end,
+					"Extract Function To File",
+				},
+				v = {
+					function()
+						refactoring.refactor("Extract Variable")
+					end,
+					"Extract Variable",
+				},
+				mode = "x",
 			},
-		}, { noremap = true, silent = true, expr = false })
+			p = modes({
+				n = {
+					function()
+						refactoring.debug.print_var({ normal = true })
+					end,
+					"Print Var",
+					mode = "n",
+				},
+				v = {
+					function()
+						refactoring.debug.print_var({})
+					end,
+					"Print Var",
+					mode = "v",
+				},
+			}),
+			-- {
+			--     debug("print_var", "{normal = true}","n"),
+			--     debug("print_var", "{}","v"),
+			-- }
+		}, { prefix = refactoring_leader, noremap = true, silent = true, expr = false })
+
 		vim.api.nvim_create_user_command("Refactoring", function(cmd)
 			if cmd.fargs[1] == "clean" then
 				require("refactoring").debug.cleanup({})
